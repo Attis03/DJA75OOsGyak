7597b122bb7db37dcf9068bd54df7a6f33a315fb
Sz-Attila03 <71133968+Sz-Attila03@users.noreply.github.com>
2021-03-16 12:18:25 +0100

    Create child.c

---
+++ DJA75O_0311/child.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("Szűcs Attila DJA75O");
+    
+    return 0;
+}
