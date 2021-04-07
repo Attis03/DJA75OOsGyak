a5efda8a33c964cf1d5ac416c40f2c29a3ab2c25
Sz-Attila03 <71133968+Sz-Attila03@users.noreply.github.com>
2021-04-07 23:19:39 +0200

    Create DJA75O_szink_sigkez.c

---
+++ DJA75O_0401/DJA75O_szink_sigkez.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t cpid;
+    cpid = fork();
+
+    if(cpid < 0)
+    {
+        printf("Sikertelen rendszer hívás!");
+    }
+    else if(cpid == 0)
+    {
+        for (alarm(20); pause() != SIGALRM;);
+    }
+    else
+    {
+        exit(1);
+    }
+
+    return 0;
+}
